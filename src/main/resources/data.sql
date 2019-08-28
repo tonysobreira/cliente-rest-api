cf4f4b480132190501625dbd4aaec66a2540ae24
Tony <tonysobreira@gmail.com>
2019-08-28 19:47:02 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,1 @@
 
---insert into cliente values(null, 'User1', '123.456.789.01', '1984-09-20', '99999-9999', 'user1@client.com', 'Rua X', '40000-000', 'Bairro X', 'Complemento X', 'Cidade X', 'Estado X');
---insert into cliente values(null, 'User2', '123.456.789.01', '1984-09-20', '99999-9999', 'user1@client.com', 'Rua X', '40000-000', 'Bairro X', 'Complemento X', 'Cidade X', 'Estado X');
---insert into cliente values(null, 'User3', '123.456.789.01', '1984-09-20', '99999-9999', 'user1@client.com', 'Rua X', '40000-000', 'Bairro X', 'Complemento X', 'Cidade X', 'Estado X');
---insert into cliente values(null, 'User4', '123.456.789.01', '1984-09-20', '99999-9999', 'user1@client.com', 'Rua X', '40000-000', 'Bairro X', 'Complemento X', 'Cidade X', 'Estado X');
---insert into cliente values(null, 'User5', '123.456.789.01', '1984-09-20', '99999-9999', 'user1@client.com', 'Rua X', '40000-000', 'Bairro X', 'Complemento X', 'Cidade X', 'Estado X');
