c9773152e0c8cc3dc59f8888e5623e51ea72cbb6
Tony <tonysobreira@gmail.com>
2019-08-28 20:13:01 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-
+insert into cliente values(1, 'Bairro X', '40000-000', 'Cidade X', 'Complemento X', '123.456.789-01', '1984-09-20', 'cliente1@cliente.com', 'Endereço X', 'Estado X', 'Cliente 1', '99999-9999');
+insert into cliente values(2, 'Bairro X', '40000-000', 'Cidade X', 'Complemento X', '123.456.789-01', '1984-09-20', 'cliente1@cliente.com', 'Endereço X', 'Estado X', 'Cliente 2', '99999-9999');
+insert into cliente values(3, 'Bairro X', '40000-000', 'Cidade X', 'Complemento X', '123.456.789-01', '1984-09-20', 'cliente1@cliente.com', 'Endereço X', 'Estado X', 'Cliente 3', '99999-9999');
+insert into cliente values(4, 'Bairro X', '40000-000', 'Cidade X', 'Complemento X', '123.456.789-01', '1984-09-20', 'cliente1@cliente.com', 'Endereço X', 'Estado X', 'Cliente 4', '99999-9999');
+insert into cliente values(5, 'Bairro X', '40000-000', 'Cidade X', 'Complemento X', '123.456.789-01', '1984-09-20', 'cliente1@cliente.com', 'Endereço X', 'Estado X', 'Cliente 5', '99999-9999');
